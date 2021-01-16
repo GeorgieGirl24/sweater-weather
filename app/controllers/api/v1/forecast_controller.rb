01350a6612ad4ff27789fa095bacad287c472973
George Soderholm <georgesoderholm@gmail.com>
2021-01-16 12:13:45 -0700

    Add a call to the forecast_serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     forecast = ForecastFacade.get_forecast(location_params)
+    render json: ForecastSerializer.new(forecast)
   end
 
   private
