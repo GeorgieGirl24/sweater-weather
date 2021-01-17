bbae0aeb70ca7018a5725b53526754a830f6036a
George Soderholm <georgesoderholm@gmail.com>
2021-01-16 22:00:49 -0700

    Add conditional for sad path errors

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    image = ImageFacade.get_image(location_params)
-    render json: ImageSerializer.new(image)
+    if location_params[:location].nil?
+
+      payload = {
+        error: invalid_location,
+        message: 'Invailid location',
+        status: 400
+      }
+
+      render json: payload, status: :bad_request
+    else
+      image = ImageFacade.get_image(location_params)
+      render json: ImageSerializer.new(image)
+    end
   end
 
   private
