930330cbd483451e1f633fe66d47e057cc801af1
George Soderholm <georgesoderholm@gmail.com>
2021-01-17 13:10:16 -0700

    Add unit test for User, including method test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password }
+    it { should validate_uniqueness_of :api_key }
+  end
+
+  describe 'callbacks' do
+    it 'will have an api_key automatically assigned when created' do
+      user_params = {
+        email: 'example@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      user = User.create(user_params)
+      expect(user.api_key).to_not be_nil
+    end
+  end
 end
