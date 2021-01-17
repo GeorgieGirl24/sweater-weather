5cf1087b269d7b9032ba75a8a488072449d89f61
George Soderholm <georgesoderholm@gmail.com>
2021-01-17 00:22:41 -0700

    Update variables

---
+++ spec/services/map_service_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe MapService, :vcr do
+  it 'can make a successful faraday call to unsplash' do
+    location = 'denver, co'
+    map = MapService.get_coordinates(location)
+
+    expect(map).to be_a Hash
+    expect(map).to have_key(:results)
+    expect(map[:results]).to be_an Array
+    expect(map[:results].first).to be_a Hash
+    expect(map[:results].first).to have_key(:locations)
+    expect(map[:results].first[:locations]).to be_an Array
+    expect(map[:results].first[:locations].first).to have_key(:latLng)
+    expect(map[:results].first[:locations].first[:latLng]).to be_a Hash
+    expect(map[:results].first[:locations].first[:latLng]).to have_key(:lat)
+    expect(map[:results].first[:locations].first[:latLng][:lat]).to be_a Float
+    expect(map[:results].first[:locations].first[:latLng][:lat]).to eq(39.738453)
+    expect(map[:results].first[:locations].first[:latLng]).to have_key(:lng)
+    expect(map[:results].first[:locations].first[:latLng][:lng]).to be_a Float
+    expect(map[:results].first[:locations].first[:latLng][:lng]).to eq(-104.984853)
+  end
+
+  it 'can fail with an error, if there external call is not possible' do
+    location = ''
+    map = MapService.get_coordinates(location)
+    expect(map).to be_a Hash
+    expect(map).to have_key(:info)
+    expect(map[:info]).to be_a Hash
+    expect(map[:info]).to have_key(:statuscode)
+    expect(map[:info][:statuscode]).to be_an Integer
+    expect(map[:info][:statuscode]).to eq(400)
+    expect(map[:info]).to have_key(:messages)
+    expect(map[:info][:messages]).to be_an Array
+    expect(map[:info][:messages].first).to eq('Illegal argument from request: Insufficient info for location')
+  end
+end
