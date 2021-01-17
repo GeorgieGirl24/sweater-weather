6c797645c9f11da7ec66711092556fcd75f66aab
George Soderholm <georgesoderholm@gmail.com>
2021-01-17 12:08:35 -0700

    Add UsersController with create method

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    ActiveRecord::Base.connection.reset_pk_sequence!('users')
+    user = User.new(user_params)
+    if user.save
+      render json: UsersSerializer.new(user), status: 201
+    else
+      render body: generate_error(user), status: 400
+    end
+binding.pry
+  end
+
+  private
+
+  def generate_error(user)
+    error = ''
+    user.errors.messages.each do |validation, message|
+      error += "#{validation} (#{message[0]});"
+    end
+  end
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
