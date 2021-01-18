f56b0773f20fe628c461004fabf65016b0737d63
George Soderholm <georgesoderholm@gmail.com>
2021-01-17 22:41:07 -0700

    Add the ability to call a serializer

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     roadtrip = RoadTripFacade.get_trip(road_trip_params)
+    render json: RoadtripSerializer.new(roadtrip)
   end
 
   private
