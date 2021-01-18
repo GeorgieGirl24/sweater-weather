b65acb00597d310579ec1fa0b5983f4cc2870924
George Soderholm <georgesoderholm@gmail.com>
2021-01-18 13:27:53 -0700

    Add sad path testing for missing/non-matching params

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 RSpec.describe 'Roadtrip Api', :vcr do
   describe 'user can create a road trip' do
     it 'successful road trip creation' do
+      User.destroy_all
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
+      user = User.create(email: 'example@email.com', password: 'password')
+
+      login_params = {
+        'email': 'example@email.com',
+        'password': 'password'
+      }
+
       road_trip_params = {
         origin: 'Denver, CO',
         destination: 'Pueblo, CO',
-        api_key: 'jgn983hy48thw9begh98h4539h4'
+        api_key: user.api_key
       }
 
       headers = {
         'CONTENT_TYPE': 'application/json',
         'ACCEPT': 'application/json'
       }
-      puts 'we are barely getting through this'
+
       post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
 
       expect(response).to be_successful
@@ -42,6 +51,98 @@ RSpec.describe 'Roadtrip Api', :vcr do
       expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a Float
       expect(road_trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
       expect(road_trip[:data][:attributes][:weather_at_eta][:conditions]).to be_a String
+    end
+
+    it 'cannot make successful road trip with missing destination_city' do
+      User.destroy_all
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
+      user = User.create(email: 'example@email.com', password: 'password')
+
+      login_params = {
+        'email': 'example@email.com',
+        'password': 'password'
+      }
+
+      road_trip_params = {
+        origin: 'Denver, CO',
+        destination: '',
+        api_key: user.api_key
+      }
+
+      headers = {
+        'CONTENT_TYPE': 'application/json',
+        'ACCEPT': 'application/json'
+      }
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(404)
+      expect(response.content_type).to eq('text/plain')
+      expect(response.body).to eq('Missing/empty fields. Please try again')
+    end
+
+    it 'cannot make successful road trip with missing api_key' do
+      User.destroy_all
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
+      user = User.create(email: 'example@email.com', password: 'password')
+
+      login_params = {
+        'email': 'example@email.com',
+        'password': 'password'
+      }
+
+      road_trip_params = {
+        origin: 'Denver, CO',
+        destination: 'Pueblo, CO',
+        api_key: ''
+      }
+
+      headers = {
+        'CONTENT_TYPE': 'application/json',
+        'ACCEPT': 'application/json'
+      }
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(401)
+      expect(response.content_type).to eq('text/plain')
+      expect(response.body).to eq('Missing/incorrect API key. Please try again')
+    end
+
+    it 'cannot make successful road trip with non-matching api_key' do
+      User.destroy_all
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
+      user = User.create(email: 'example@email.com', password: 'password')
+
+      login_params = {
+        'email': 'example@email.com',
+        'password': 'password'
+      }
+
+      expect(user.email).to eq('example@email.com')
+      expect(user.password_digest).to be_a String
+      expect(user.api_key).to be_a String
+
+      road_trip_params = {
+        origin: 'Denver, CO',
+        destination: 'Pueblo, CO',
+        api_key: 'jgn983hy48thw9begh98h4539h4'
+      }
+
+      headers = {
+        'CONTENT_TYPE': 'application/json',
+        'ACCEPT': 'application/json'
+      }
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
+
+      expect(user.api_key).to_not eq(road_trip_params[:api_key])
+      expect(response).to_not be_successful
+      expect(response.status).to be(401)
+      expect(response.content_type).to eq('text/plain')
+      expect(response.body).to eq('Missing/incorrect API key. Please try again')
     end
   end
 end
