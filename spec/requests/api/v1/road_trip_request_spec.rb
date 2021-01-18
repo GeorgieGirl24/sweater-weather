e2f545aab40172e8a6248a1e25e427575cd7f636
George Soderholm <georgesoderholm@gmail.com>
2021-01-17 22:36:55 -0700

    Update the keys to be correct for info extraction

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
-RSpec.describe 'RoadTrip Api', :vcr do
+RSpec.describe 'Roadtrip Api', :vcr do
   describe 'user can create a road trip' do
     it 'successful road trip creation' do
-
-# binding.pry
       road_trip_params = {
         origin: 'Denver, CO',
         destination: 'Pueblo, CO',
@@ -25,25 +23,25 @@ RSpec.describe 'RoadTrip Api', :vcr do
       expect(road_trip).to be_a Hash
       expect(road_trip).to have_key(:data)
       expect(road_trip[:data]).to be_a Hash
-      expect(road_trip).to have_key(:type)
-      expect(road_trip[:type]).to be_a String
-      expect(road_trip[:type]).to eq('roadtrip')
-      expect(road_trip).to have_key(:id)
-      expect(road_trip[:id]).to be_nil
-      expect(road_trip).to have_key(:attributes)
-      expect(road_trip[:type][:attributes]).to be_a Hash
-      expect(road_trip[:type][:attributes]).to have_key(:start_city)
-      expect(road_trip[:type][:attributes][:start_city]).to be_a String
-      expect(road_trip[:type][:attributes]).to have_key(:end_city)
-      expect(road_trip[:type][:attributes][:end_city]).to be_a String
-      expect(road_trip[:type][:attributes]).to have_key(:travel_time)
-      expect(road_trip[:type][:attributes][:travel_time]).to be_a String
-      expect(road_trip[:type][:attributes]).to have_key(:weather_at_eta)
-      expect(road_trip[:type][:attributes][:weather_at_eta]).to be_a Hash
-      expect(road_trip[:type][:attributes][:weather_at_eta]).to have_key(:temperature)
-      expect(road_trip[:type][:attributes][:weather_at_eta][:temperature]).to be_a Float
-      expect(road_trip[:type][:attributes][:weather_at_eta]).to have_key(:conditions)
-      expect(road_trip[:type][:attributes][:weather_at_eta][:conditions]).to be_a String
+      expect(road_trip[:data]).to have_key(:type)
+      expect(road_trip[:data][:type]).to be_a String
+      expect(road_trip[:data][:type]).to eq('roadtrip')
+      expect(road_trip[:data]).to have_key(:id)
+      expect(road_trip[:data][:id]).to be_nil
+      expect(road_trip[:data]).to have_key(:attributes)
+      expect(road_trip[:data][:attributes]).to be_a Hash
+      expect(road_trip[:data][:attributes]).to have_key(:start_city)
+      expect(road_trip[:data][:attributes][:start_city]).to be_a String
+      expect(road_trip[:data][:attributes]).to have_key(:end_city)
+      expect(road_trip[:data][:attributes][:end_city]).to be_a String
+      expect(road_trip[:data][:attributes]).to have_key(:travel_time)
+      expect(road_trip[:data][:attributes][:travel_time]).to be_a String
+      expect(road_trip[:data][:attributes]).to have_key(:weather_at_eta)
+      expect(road_trip[:data][:attributes][:weather_at_eta]).to be_a Hash
+      expect(road_trip[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+      expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a Float
+      expect(road_trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+      expect(road_trip[:data][:attributes][:weather_at_eta][:conditions]).to be_a String
     end
   end
 end
