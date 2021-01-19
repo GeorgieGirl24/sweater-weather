55b95fb3722ed059163688e081b6ba068ae538f9
George Soderholm <georgesoderholm@gmail.com>
2021-01-18 18:31:14 -0700

    Update how to get the params from the body

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,6 +19,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    JSON.parse(request.raw_post)
   end
 end
