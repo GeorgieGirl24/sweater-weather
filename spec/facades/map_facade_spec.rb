f90dd994bff4d44c5e87848b63f96be9a1d6d927
George Soderholm <georgesoderholm@gmail.com>
2021-01-18 19:37:30 -0700

    Update another nested hash key to be accurate

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -21,31 +21,33 @@ RSpec.describe MapFacade, :vcr do
     destination = 'Pueblo, CO'
     trip_duration = MapFacade.get_trip(origin, destination)
     expect(trip_duration).to be_a Hash
-    expect(trip_duration).to have_key(:boundingBox)
-    expect(trip_duration[:boundingBox]).to be_a Hash
-    expect(trip_duration[:boundingBox]).to have_key(:lr)
-    expect(trip_duration[:boundingBox][:lr]).to be_a Hash
-    expect(trip_duration[:boundingBox][:lr]).to have_key(:lng)
-    expect(trip_duration[:boundingBox][:lr][:lng]).to be_a Float
-    expect(trip_duration[:boundingBox][:lr][:lng]).to eq(-104.605087)
-    expect(trip_duration[:boundingBox][:lr]).to have_key(:lat)
-    expect(trip_duration[:boundingBox][:lr][:lat]).to be_a Float
-    expect(trip_duration[:boundingBox][:lr][:lat]).to eq(38.265427)
-    expect(trip_duration[:boundingBox]).to have_key(:ul)
-    expect(trip_duration[:boundingBox][:ul]).to be_a Hash
-    expect(trip_duration[:boundingBox][:ul]).to have_key(:lng)
-    expect(trip_duration[:boundingBox][:ul][:lng]).to be_a Float
-    expect(trip_duration[:boundingBox][:ul][:lng]).to eq(-104.98761)
-    expect(trip_duration[:boundingBox][:ul]).to have_key(:lat)
-    expect(trip_duration[:boundingBox][:ul][:lat]).to be_a Float
-    expect(trip_duration[:boundingBox][:ul][:lat]).to eq(39.738453)
-    expect(trip_duration).to have_key(:realTime)
-    expect(trip_duration[:realTime]).to be_an Integer
-    expect(trip_duration[:realTime]).to eq(6643)
-    expect(trip_duration).to have_key(:formattedTime)
-    expect(trip_duration[:formattedTime]).to be_an String
-    expect(trip_duration[:formattedTime]).to eq('01:44:22')
-    expect(trip_duration).to have_key(:routeError)
-    expect(trip_duration[:routeError]).to be_an Hash
+    expect(trip_duration).to have_key(:route)
+    expect(trip_duration[:route]).to be_a Hash
+    expect(trip_duration[:route]).to have_key(:boundingBox)
+    expect(trip_duration[:route][:boundingBox]).to be_a Hash
+    expect(trip_duration[:route][:boundingBox]).to have_key(:lr)
+    expect(trip_duration[:route][:boundingBox][:lr]).to be_a Hash
+    expect(trip_duration[:route][:boundingBox][:lr]).to have_key(:lng)
+    expect(trip_duration[:route][:boundingBox][:lr][:lng]).to be_a Float
+    expect(trip_duration[:route][:boundingBox][:lr][:lng]).to eq(-104.605087)
+    expect(trip_duration[:route][:boundingBox][:lr]).to have_key(:lat)
+    expect(trip_duration[:route][:boundingBox][:lr][:lat]).to be_a Float
+    expect(trip_duration[:route][:boundingBox][:lr][:lat]).to eq(38.265427)
+    expect(trip_duration[:route][:boundingBox]).to have_key(:ul)
+    expect(trip_duration[:route][:boundingBox][:ul]).to be_a Hash
+    expect(trip_duration[:route][:boundingBox][:ul]).to have_key(:lng)
+    expect(trip_duration[:route][:boundingBox][:ul][:lng]).to be_a Float
+    expect(trip_duration[:route][:boundingBox][:ul][:lng]).to eq(-104.98761)
+    expect(trip_duration[:route][:boundingBox][:ul]).to have_key(:lat)
+    expect(trip_duration[:route][:boundingBox][:ul][:lat]).to be_a Float
+    expect(trip_duration[:route][:boundingBox][:ul][:lat]).to eq(39.738453)
+    expect(trip_duration[:route]).to have_key(:realTime)
+    expect(trip_duration[:route][:realTime]).to be_an Integer
+    expect(trip_duration[:route][:realTime]).to eq(6643)
+    expect(trip_duration[:route]).to have_key(:formattedTime)
+    expect(trip_duration[:route][:formattedTime]).to be_an String
+    expect(trip_duration[:route][:formattedTime]).to eq('01:44:22')
+    expect(trip_duration[:route]).to have_key(:routeError)
+    expect(trip_duration[:route][:routeError]).to be_an Hash
   end
 end
