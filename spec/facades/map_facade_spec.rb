865c2f3f3f6c4156647b308370c6c6cd3bb024f8
George Soderholm <georgesoderholm@gmail.com>
2021-01-18 18:29:35 -0700

    Add specifics to test and expectations about what keys I execpt

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -15,4 +15,37 @@ RSpec.describe MapFacade, :vcr do
     expect(map.longitude).to be_an Float
     expect(map.longitude).to eq(-104.984853)
   end
+
+  it 'can take an origin city and end city and find trip' do
+    origin = 'Denver, CO'
+    destination = 'Pueblo, CO'
+    trip_duration = MapFacade.get_trip(origin, destination)
+    expect(trip_duration).to be_a Hash
+    expect(trip_duration).to have_key(:boundingBox)
+    expect(trip_duration[:boundingBox]).to be_a Hash
+    expect(trip_duration[:boundingBox]).to have_key(:lr)
+    expect(trip_duration[:boundingBox][:lr]).to be_a Hash
+    expect(trip_duration[:boundingBox][:lr]).to have_key(:lng)
+    expect(trip_duration[:boundingBox][:lr][:lng]).to be_a Float
+    expect(trip_duration[:boundingBox][:lr][:lng]).to eq(-104.605087)
+    expect(trip_duration[:boundingBox][:lr]).to have_key(:lat)
+    expect(trip_duration[:boundingBox][:lr][:lat]).to be_a Float
+    expect(trip_duration[:boundingBox][:lr][:lat]).to eq(38.265427)
+    expect(trip_duration[:boundingBox]).to have_key(:ul)
+    expect(trip_duration[:boundingBox][:ul]).to be_a Hash
+    expect(trip_duration[:boundingBox][:ul]).to have_key(:lng)
+    expect(trip_duration[:boundingBox][:ul][:lng]).to be_a Float
+    expect(trip_duration[:boundingBox][:ul][:lng]).to eq(-104.98761)
+    expect(trip_duration[:boundingBox][:ul]).to have_key(:lat)
+    expect(trip_duration[:boundingBox][:ul][:lat]).to be_a Float
+    expect(trip_duration[:boundingBox][:ul][:lat]).to eq(39.738453)
+    expect(trip_duration).to have_key(:realTime)
+    expect(trip_duration[:realTime]).to be_an Integer
+    expect(trip_duration[:realTime]).to eq(6643)
+    expect(trip_duration).to have_key(:formattedTime)
+    expect(trip_duration[:formattedTime]).to be_an String
+    expect(trip_duration[:formattedTime]).to eq('01:44:22')
+    expect(trip_duration).to have_key(:routeError)
+    expect(trip_duration[:routeError]).to be_an Hash
+  end
 end
