87995e618cc1b146794a14967573a68f06fcca60
George Soderholm <georgesoderholm@gmail.com>
2021-01-19 20:14:29 -0700

    Add MunchiesService

---
+++ app/services/munchies_service.rb
@@ -0,0 +1,21 @@
+class MunchiesService
+  def self.get_restaurants(destination, term)
+    response = conn.get('/v3/businesses/search') do |req|
+      req.params['location'] = destination
+      req.params['term'] = term
+      req.params['open_now'] = true
+    end
+    parse_data(response)
+  end
+
+  def self.parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def self.conn
+    Faraday.new('https://api.yelp.com') do |req|
+    req.headers["Authorization"] = "Bearer #{ENV['YELP_API_KEY']}"
+    end
+  end
+end
