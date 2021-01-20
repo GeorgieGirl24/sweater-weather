750d9d5793e031c5bef3f4b7de3deabd40f9c97b
George Soderholm <georgesoderholm@gmail.com>
2021-01-19 18:18:30 -0700

    Add vcrs

---
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'Image API', :vcr do
+  describe 'it can get a background image based on the city search' do
+    it 'can get an image from Unsplash' do
+      location_params = {
+        location: 'denver, co'
+      }
+
+      get '/api/v1/backgrounds', params: location_params
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
+
+      image = JSON.parse(response.body, symbolize_names: true)[:data]
+# binding.pry
+      expect(image).to be_a Hash
+      expect(image).to have_key(:id)
+      expect(image[:id]).to be_nil
+      expect(image).to have_key(:type)
+      expect(image[:type]).to eq('image')
+      expect(image).to have_key(:attributes)
+      expect(image[:attributes]).to be_a Hash
+      expect(image[:attributes]).to have_key(:credit)
+      expect(image[:attributes][:credit]).to be_an Hash
+      expect(image[:attributes][:credit]).to have_key(:artist)
+      expect(image[:attributes][:credit][:artist]).to be_a String
+      expect(image[:attributes][:credit]).to have_key(:artist_link)
+      expect(image[:attributes][:credit][:artist_link]).to be_a String
+      expect(image[:attributes][:credit]).to have_key(:source)
+      expect(image[:attributes][:credit][:source]).to be_a String
+      expect(image[:attributes]).to have_key(:image)
+      expect(image[:attributes][:image]).to be_an Hash
+      expect(image[:attributes][:image]).to have_key(:image_url)
+      expect(image[:attributes][:image][:image_url]).to be_a String
+      expect(image[:attributes][:image]).to have_key(:link)
+      expect(image[:attributes][:image][:link]).to be_a String
+      expect(image[:attributes][:image]).to have_key(:location)
+      expect(image[:attributes][:image][:location]).to be_a Hash
+      expect(image[:attributes]).to_not be_an Array
+      expect(image[:attributes][:image]).to_not be_an Array
+    end
+
+    it 'cannot get an Image without a location' do
+      location_params = {
+        location: ''
+      }
+
+      get '/api/v1/backgrounds', params: location_params
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to eq("{\"error\":\"Unable to find image without location\"}")
+    end
+  end
+end
