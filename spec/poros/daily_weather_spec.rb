8142bd0f2ce77a8eb4139d0da34ae20186dcd80a
George Soderholm <georgesoderholm@gmail.com>
2021-01-20 02:50:18 -0700

    Reduce width

--- spec/poros/daily_weather_spec.rb
+++ spec/poros/daily_weather_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe DailyWeather, :vcr do
     expect(many_day_weather.first.sunrise).to eq(sunrise)
     expect(many_day_weather.first.sunrise).to_not eq(1_610_056_347)
     sunset = '2021-01-16 17:00:41.000000000 -0700'
-    expect(many_day_weather.first.sunset).to eq()
+    expect(many_day_weather.first.sunset).to eq(sunset)
     expect(many_day_weather.first.sunset).to_not eq(1_610_092_098)
     expect(many_day_weather.first.max_temp).to eq(47.71)
     expect(many_day_weather.first.max_temp).to_not eq(274.74) #Kelvin
